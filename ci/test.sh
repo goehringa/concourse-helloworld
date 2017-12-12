83e68a727eef9e49b8b77218d0b76f9b4f48f086
goehringa <ahgoehring@gmail.com>
2017-12-12 00:48:18 -0500

    Fix directory name

--- ci/test.sh
+++ ci/test.sh
@@ -5,7 +5,7 @@ set -e -x
 yum -y update && yum clean all
 yum -y install epel-release && yum clean all
 yum -y install python-pip && yum clean all
-pushd gannett
+pushd helloworld
   pip install --trusted-host pypi.python.org -r requirements.txt
   python helloworld_tests.py
 popd
