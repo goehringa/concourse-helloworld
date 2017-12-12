8238654fca326e535a6501ea249a065715a42716
goehringa <ahgoehring@gmail.com>
2017-12-12 16:10:21 -0500

    Change path to requirements.txt back

--- ci/test.sh
+++ ci/test.sh
@@ -6,6 +6,6 @@ yum -y update && yum clean all
 yum -y install epel-release && yum clean all
 yum -y install python-pip && yum clean all
 pushd helloworld/docker
-  pip install --trusted-host pypi.python.org -r docker/requirements.txt
+  pip install --trusted-host pypi.python.org -r requirements.txt
   python helloworld_tests.py
 popd
