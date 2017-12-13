8a9b05bd9ef2e85de9ef6c44e1be42a1744b9c43
goehringa <ahgoehring@gmail.com>
2017-12-13 15:47:30 -0500

    refactor flask code

--- docker/helloworld.py
+++ docker/helloworld.py
@@ -2,21 +2,14 @@ from flask import Flask
 from flask import request
 
 app = Flask(__name__)
-
+  
 @app.route('/', methods=['GET'])
 def helloworld():
   response = 'hello world'
   uppercase = request.args.get('uppercase')
   reversed = request.args.get('reversed')
-  if uppercase == 'true' and reversed == 'true':
-    response = response.upper()
-    response = response[::-1]
-  elif uppercase == 'true':
-    response = response.upper()
-  elif reversed == 'true':
-    response = response[::-1]
-  else:
-    response = 'hello world'
+
+  response = format_response(response, uppercase, reversed)
   return response
 
 @app.route('/hello', methods=['GET'])
@@ -24,14 +17,8 @@ def hello():
   response = 'hello'
   uppercase = request.args.get('uppercase')
   reversed = request.args.get('reversed')
-  if uppercase == 'true' and reversed == 'true':
-    response = response.upper()
-    response = response[::-1]
-  elif uppercase == 'true':
-    response = response.upper()
-  elif reversed == 'true':
-    response = response[::-1]
   
+  response = format_response(response, uppercase, reversed)
   return response
 
 @app.route('/world', methods=['GET'])
@@ -39,6 +26,11 @@ def world():
   response = 'world'
   uppercase = request.args.get('uppercase')
   reversed = request.args.get('reversed')
+  
+  response = format_response(response, uppercase, reversed)
+  return response
+
+def format_response(response, uppercase, reversed):
   if uppercase == 'true' and reversed == 'true':
     response = response.upper()
     response = response[::-1]
