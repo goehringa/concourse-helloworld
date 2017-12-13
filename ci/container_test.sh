e9d8c492c94d86f29074e88797a82d683bb8fae2
goehringa <ahgoehring@gmail.com>
2017-12-12 19:54:11 -0500

    Install docker on centos image

--- ci/container_test.sh
+++ ci/container_test.sh
@@ -2,13 +2,26 @@
 
 set -e -x
 
+yum install -y yum-utils \
+  device-mapper-persistent-data \
+  lvm2
+
+yum-config-manager \
+  --add-repo \
+  https://download.docker.com/linux/centos/docker-ce.repo
+
+yum install docker-ce
+systemctl start docker
+
 curl \
   -L https://github.com/aelsabbahy/goss/releases/download/_VERSION_/goss-linux-amd64 \
   -o /usr/local/bin/goss
+
 chmod +rx /usr/local/bin/goss
 curl \
   -L https://raw.githubusercontent.com/aelsabbahy/goss/_VERSION_/extras/dgoss/dgoss \
   -o /usr/local/bin/dgoss
+
 chmod +rx /usr/local/bin/dgoss
 
 pushd helloworld/docker
